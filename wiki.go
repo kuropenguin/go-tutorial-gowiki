3a78400e9df44e46d8f12bde95ba3eae1a72a95c
kuropenguin <kuro1994.t@gmail.com>
2023-01-19 16:55:15 +0900

    implement save test page

--- wiki.go
+++ wiki.go
@@ -1,6 +1,16 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
 
 type Page struct {
 	Title string
